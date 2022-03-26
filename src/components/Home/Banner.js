fdcf622a8d295a62f15329d43d4babd57fc63963
Volodymyr Vovk <volodymyr.vovk@uproartech.com>
2022-03-26 15:22:59 +0200

    [GitHub]: try if correct

--- src/components/Home/Banner.js
+++ src/components/Home/Banner.js
@@ -10,7 +10,8 @@ const Banner = ({ appName, token }) => {
         <h1 className="logo-font">
           {appName.toLowerCase()}
         </h1>
-        <p>A place to share your knowledge.</p>
+        {/* <p>A place to share your knowledge.</p> */}
+        <p>Looooooooooooooool</p>
       </div>
     </div>
   );
